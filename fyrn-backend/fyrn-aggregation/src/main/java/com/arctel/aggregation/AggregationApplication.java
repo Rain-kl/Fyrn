6b855651d185850cf5152b8574cd507f180f2fcd
ryan <ryan@arctel.net>
2025-12-13 20:11:52 +0800

    基础目录结构

---
+++ fyrn-backend/fyrn-aggregation/src/main/java/com/arctel/aggregation/AggregationApplication.java
@@ -0,0 +1,13 @@
+package com.arctel.aggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class AggregationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregationApplication.class, args);
+    }
+
+}
---
+++ fyrn-backend/fyrn-biz-mms/src/main/java/com/arctel/mms/MmsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arctel.mms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class MmsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmsServiceApplication.class, args);
+    }
+
+}
