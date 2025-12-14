39985559a0c68c3887ed90f485bdaa5f517c25d2
ryan <ryan@arctel.net>
2025-12-14 11:56:36 +0800

    AggregationApplication 启动

--- fyrn-backend/fyrn-aggregation/src/main/java/com/arctel/aggregation/AggregationApplication.java
+++ fyrn-backend/fyrn-aggregation/src/main/java/com/arctel/aggregation/AggregationApplication.java
@@ -17,10 +17,14 @@
 
 package com.arctel.aggregation;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.arctel.mms.mapper")
+@ComponentScan(basePackages = {"com.arctel.mms"})
 class AggregationApplication {
 
     public static void main(String[] args) {
