cf91842f42ef0c3e14c61c1d4da2b22b688893f4
ryan <ryan@arctel.net>
2025-12-31 16:21:32 +0800

    feat: fix some bugs

--- fyrn-backend/fyrn-oms/oms-biz-impl/src/main/java/com/arctel/oms/biz/OmsAuthServiceImpl.java
+++ fyrn-backend/fyrn-oms/oms-biz-impl/src/main/java/com/arctel/oms/biz/OmsAuthServiceImpl.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.arctel.oms.biz;
 
 import cn.hutool.crypto.digest.BCrypt;
@@ -10,6 +27,8 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 /**
  * @author hspcadmin
  * @description 针对表【oms_user(系统用户表)】的数据库操作Service
@@ -20,6 +39,25 @@ public class OmsAuthServiceImpl implements OmsAuthService {
 
     @Resource
     private OmsUserMapper omsUserMapper;
+
+
+    @Override
+    public Boolean login(String userId, String password) {
+        passCheck(password);
+        OmsUser omsUser = omsUserMapper.selectOne(
+                new LambdaQueryWrapper<OmsUser>()
+                        .eq(OmsUser::getUserId, userId));
+        if (omsUser == null) {
+            throw new BizException(ErrorConstant.USER_NOT_FOUND, "用户不存在");
+        }
+        if (!BCrypt.checkpw(password, omsUser.getPassword())) {
+            throw new BizException(ErrorConstant.COMMON_ERROR, "密码错误");
+        }
+        omsUser.setLastLoginTime(new Date());
+        omsUserMapper.updateById(omsUser);
+        return true;
+    }
+
 
     @Override
     public String updatePassowrd(String userId, String password) {
@@ -34,21 +72,6 @@ public class OmsAuthServiceImpl implements OmsAuthService {
         omsUser.setPassword(hashPasswd);
         omsUserMapper.updateById(omsUser);
         return userId;
-    }
-
-    @Override
-    public Boolean login(String userId, String password) {
-        passCheck(password);
-        OmsUser omsUser = omsUserMapper.selectOne(
-                new LambdaQueryWrapper<OmsUser>()
-                        .eq(OmsUser::getUserId, userId));
-        if (omsUser == null) {
-            throw new BizException(ErrorConstant.USER_NOT_FOUND, "用户不存在");
-        }
-        if (!BCrypt.checkpw(password, omsUser.getPassword())) {
-            throw new BizException(ErrorConstant.COMMON_ERROR, "密码错误");
-        }
-        return true;
     }
 
 
--- fyrn-backend/fyrn-oms/oms-biz-impl/src/main/java/com/arctel/oms/biz/OmsUserServiceImpl.java
+++ fyrn-backend/fyrn-oms/oms-biz-impl/src/main/java/com/arctel/oms/biz/OmsUserServiceImpl.java
@@ -1,20 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.arctel.oms.biz;
 
+import com.arctel.oms.biz.auth.OmsAuthService;
 import com.arctel.oms.biz.mapper.OmsUserMapper;
 import com.arctel.oms.biz.user.OmsUserService;
 import com.arctel.oms.pub.base.BaseQueryPage;
+import com.arctel.oms.pub.constants.ErrorConstant;
 import com.arctel.oms.pub.domain.OmsUser;
 import com.arctel.oms.pub.domain.input.QueryUserPageInput;
 import com.arctel.oms.pub.domain.input.UserCreateInput;
 import com.arctel.oms.pub.domain.input.UserUpdateInput;
 import com.arctel.oms.pub.domain.output.UserInfoVo;
+import com.arctel.oms.pub.exception.BizException;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
+import jakarta.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -27,13 +50,23 @@ import java.util.List;
 public class OmsUserServiceImpl extends ServiceImpl<OmsUserMapper, OmsUser>
         implements OmsUserService {
 
+    @Resource
+    OmsAuthService omsAuthService;
+
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public String createUser(UserCreateInput input) {
+        String username = input.getUsername();
+        String password = input.getPassword();
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+            throw new BizException(ErrorConstant.CHECK_FAILED, "用户名和密码不能为空");
+        }
         OmsUser omsUser = new OmsUser();
         BeanUtils.copyProperties(input, omsUser);
         // 安全因素, 修改密码操作使用专用接口
         omsUser.setPassword(null);
         this.save(omsUser);
+        omsAuthService.updatePassowrd(omsUser.getUserId(), input.getPassword());
         return omsUser.getUserId();
     }
 
@@ -73,11 +106,18 @@ public class OmsUserServiceImpl extends ServiceImpl<OmsUserMapper, OmsUser>
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public String updateUser(UserUpdateInput input) {
         String userId = input.getUserId();
+        if (StringUtils.isBlank(userId)) {
+            throw new BizException(ErrorConstant.CHECK_FAILED, "用户ID不能为空");
+        }
         OmsUser omsUser = getById(userId);
         BeanUtils.copyProperties(input, omsUser);
         omsUser.setPassword(null);
+        if (StringUtils.isNotBlank(input.getPassword())) {
+            omsAuthService.updatePassowrd(input.getUserId(), input.getPassword());
+        }
         this.updateById(omsUser);
         return omsUser.getUserId();
     }
